3279a7c03d0cf115674fa8335e0d4b5acf71e486
Ekaterina Kloss <kloss-kate@mail.ru>
2022-12-05 18:23:42 +0300

    Task 2 completed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿int CutNumber(int num)
+﻿/*
+int CutNumber(int num)
 {
     int des = num / 10;
   
@@ -10,3 +11,35 @@ Console.Write("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int newNumber = CutNumber(number);
 Console.WriteLine($"Вторая цифра из числа {number}: {newNumber} ");
+*/
+
+int CutNum(int num)
+{
+while(num > 999)
+{
+    num = num / 10;
+}
+int result = num % 10;
+int none = -1;
+
+if (num > 99)
+{
+    return result;
+}
+else
+{
+    return none;
+}
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int thirdNumber = CutNum(number);
+
+if (thirdNumber == -1)
+{
+    Console.WriteLine("Третьего числа нет ");
+}
+else
+{
+    Console.WriteLine($"Третья цифра из числа {number}: {thirdNumber} ");
+}
