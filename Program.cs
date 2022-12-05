3b7b8800615a137e75312514d00bebe0d2211be8
Ekaterina Kloss <kloss-kate@mail.ru>
2022-12-05 18:38:53 +0300

    Task 3 completed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿/*
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
 int CutNumber(int num)
 {
     int des = num / 10;
@@ -13,6 +14,8 @@ int newNumber = CutNumber(number);
 Console.WriteLine($"Вторая цифра из числа {number}: {newNumber} ");
 */
 
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*
 int CutNum(int num)
 {
 while(num > 999)
@@ -37,9 +40,24 @@ int thirdNumber = CutNum(number);
 
 if (thirdNumber == -1)
 {
-    Console.WriteLine("Третьего числа нет ");
+    Console.WriteLine("Третьей цифры нет ");
 }
 else
 {
     Console.WriteLine($"Третья цифра из числа {number}: {thirdNumber} ");
 }
+*/
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+bool WeekEnd(int num)
+{
+    if (num == 6 || num == 7)
+    return true;
+    else
+    return false;
+}
+Console.Write("Введите цифру: ");
+int day = Convert.ToInt32(Console.ReadLine());
+bool result = WeekEnd(day);
+Console.WriteLine(result);
