2ff26642171f9655156083106b70305609f25f67
Ekaterina Kloss <kloss-kate@mail.ru>
2022-12-05 13:09:48 +0300

    First task completed

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int CutNumber(int num)
+{
+    int des = num / 10;
+  
+    int result = des % 10;
+    return result;
+}
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int newNumber = CutNumber(number);
+Console.WriteLine($"Вторая цифра из числа {number}: {newNumber} ");
